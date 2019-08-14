95e2e0477c1bca0f991c44b20c9375718004430c
vkorchynskyi <v.korchynskyy@gmail.com>
2019-08-14 16:23:44 +0300

    Hello Express

--- myApp.js
+++ myApp.js
@@ -2,7 +2,6 @@
 var express = require('express');
 var app = express();
 
-console.log("Hello World");
 
 // --> 7)  Mount the Logger middleware here
 
@@ -11,10 +10,12 @@ console.log("Hello World");
 
 
 /** 1) Meet the node console. */
-
+console.log("Hello World");
 
 /** 2) A first working Express Server */
-
+app.get("/", function(req, res) {
+    res.send("Hello Express")
+})
 
 /** 3) Serve an HTML file */
 
