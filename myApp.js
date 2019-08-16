10edb8af5ef1e96639da43b1e6a1ece706a6a509
vkorchynskyi <v.korchynskyy@gmail.com>
2019-08-16 20:34:16 +0300

    post data

--- myApp.js
+++ myApp.js
@@ -1,22 +1,15 @@
-
 var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
 
-// --> 7)  Mount the Logger middleware here
-
-
-// --> 11)  Mount the body-parser middleware  here
-
-
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(function middleware(req, res, next) {
+app.use(function(req, res, next) {
     const message = `${req.method} ${req.path} - ${req.ip}`
     console.log(message);
     next();
 })
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
 
 /** Serve an HTML file */
 app.get("/", function(req, res) {
@@ -51,17 +44,12 @@ app.get("/name", function(req, res) {
     res.json({name})
 });
 
-/** 10) Get input from client - Query parameters */
-// /name?first=<firstname>&last=<lastname>
-
-  
-/** 11) Get ready for POST Requests - the `body-parser` */
-// place it before all the routes !
-
-
-/** 12) Get data form POST  */
-
-
+/** post name from front page */
+app.post("/name", function(req, res) {
+    const { first, last } = req.body;
+    const name = `${first} ${last}`
+    res.json({name})
+});
 
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
